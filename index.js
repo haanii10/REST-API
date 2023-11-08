925e89a866c5bce149275fe539e8d4c84a53f787
Honeyleth A. Amores <amoreshoneyleth1803@gmail.com>
2023-11-08 13:54:25 -0800

    First Commit

--- index.js
+++ index.js
@@ -5,20 +5,15 @@ dotenv.config();
 
 const { API_URL } = process.env;
 const app = Express();
-const router = Router();
 
-app.use(json()).use(
-  router.get("/", function (req, res) {
-    return res.send("Hello from rest-api v2");
-  }),
-  router.post("/post-request/:ambot", (req, res) => {
-    const body = req.body;
-    const params = req.params;
-    const queryParams = req.query;
+//routers
+import userRouter from "./src/modules/user/user.route.js";
 
-    res.status(200).json({ Body: body, Params: params, "Query params": queryParams });
-  }),
-);
+app.use(json())
+.use(`${API_URL}users`, userRouter) //localhost:3000/api/v1/users
+.use("*", (req, res) => {
+  return res.status(404).send("Not Found...");
+});
 
 app.listen(3000, function () {
   console.log(API_URL);
